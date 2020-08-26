1806d1b3f1d4e639717976986d39edf1e0155b40
driftingdream <ezrage2001@outlook.com>
2020-08-26 11:37:31 +0800

    update main

--- main.cpp
+++ main.cpp
@@ -10,9 +10,44 @@ using json = nlohmann::json;
 std::string GIST_ID = getenv("GIST_ID");
 std::string GH_TOKEN = getenv("GH_TOKEN");
 
+void updateGist(std::string fileName, std::string content)
+{
+    using namespace curlpp::options;
+    std::list<std::string> headers;
+    std::string gistUrl = "https://api.github.com/gists/";
+    gistUrl += GIST_ID;
+    headers.push_back("Content-Type: application/json");
+    std::string auth = "Authorization: token ";
+    auth.append(GH_TOKEN);
+    headers.push_back(auth);
+    headers.push_back("User-Agent: twitter_trend");
+    curlpp::Easy requests;
+    json jsonData = R"(
+        {
+            "files":
+            {
+
+            }
+        }
+    )"_json;
+    jsonData["files"][fileName]["content"] = content;
+    std::string postData = jsonData.dump();
+    std::ostringstream os;
+    requests.setOpt(new WriteStream(&os));
+    // requests.setOpt(new Verbose(true));
+    requests.setOpt(new Url(gistUrl));
+    requests.setOpt(new SslEngineDefault());
+    requests.setOpt(new HttpHeader(headers));
+    requests.setOpt(new PostFields(postData));
+    requests.setOpt(new PostFieldSize(postData.length()));
+    requests.perform();
+    
+    
+}
+
 int main()
 {
-    std::string baseTwitterUrl = "https://api.twitter.com/1.1/trends/place.json?id=";
+    std::string baseTwitterUrl = "https://api.twitter.com/1.1/trends/place.json?id=23424977";
     std::list<std::string> headers;
 
     std::ostringstream os;
@@ -41,11 +76,13 @@ int main()
         string twitterTrend;
         for (int i = 0; i < j[0]["trends"].size(); i++)
         {
+            twitterTrend += to_string(i+1) + ' ';
             twitterTrend += j[0]["trends"][i]["name"];
             twitterTrend += "\n";
         }
         twitterTrend.erase(twitterTrend.size() - 1, 1);
-
+        // cout << twitterTrend << endl;
+        updateGist("Twitter Trending(USA).txt", twitterTrend);
     }
     catch (curlpp::LogicError &e)
     {
