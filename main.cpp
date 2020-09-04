db6d6e892ef4d05da91e7e11e9ca36745e95a10e
driftingdream <ezrage2001@outlook.com>
2020-09-04 11:57:07 +0800

    enhance: realize basic function

--- main.cpp
+++ main.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <sstream>
 #include <cstdlib>
+#include <thread>
+#include <chrono>
 #include <curlpp/cURLpp.hpp>
 #include <curlpp/Easy.hpp>
 #include <curlpp/Exception.hpp>
@@ -36,9 +38,16 @@ Dict::Dict(std::string country, std::string countryID)
 Dict::~Dict()
 {
 }
+Dict worldDict[] = {
+    Dict("USA", "23424977"),
+    Dict("UK", "23424975"),
+    Dict("World", "1"),
+    Dict("Belarus", "23424765"),
+    Dict("Australia", "23424748")};
 
+int length = sizeof(worldDict) / sizeof(worldDict[0]);
 
-void updateGist(std::string fileName, std::string content)
+void updateGist(std::string fileName, std::string & content)
 {
     using namespace curlpp::options;
     std::list<std::string> headers;
@@ -73,43 +82,54 @@ void updateGist(std::string fileName, std::string content)
 
 int main()
 {
-    std::string baseTwitterUrl = "https://api.twitter.com/1.1/trends/place.json?id=23424977";
-    std::list<std::string> headers;
-
-    std::ostringstream os;
-    curlpp::options::WriteStream ws(&os);
+    std::string baseTwitterUrl = "https://api.twitter.com/1.1/trends/place.json?id=";
+    std::stringstream fileNameStream;
+    std::string fileName;
+    
 
     using namespace curlpp::Options;
     using namespace std;
-    std::string headerData = "Authorization: Bearer ";
     try
     {
-        headerData.append(std::getenv("BEARER_TOKEN"));
-        curlpp::Cleanup cleaner;
-        curlpp::Easy request;
-        std::list<std::string> headers;
-        headers.push_back(headerData);
-        request.setOpt(new HttpHeader(headers));
-        request.setOpt(new Url(baseTwitterUrl));
-        request.setOpt(ws);
-        // request.setOpt(new Verbose(true));
-        request.perform();
-
-        string responseText = os.str();
-        // cout << responseText << endl;
-        json j = json::parse(responseText);
-        string twitterTrend;
-        for (int i = 0; i < j[0]["trends"].size(); i++)
+        for(int i = 0; i < length; i++)
         {
-            twitterTrend += to_string(i + 1) + ' ';
-            twitterTrend += j[0]["trends"][i]["name"];
-            twitterTrend += "    ";
-            twitterTrend += to_string(j[0]["trends"][i]["tweet_volume"]);
-            twitterTrend += "\n";
+            std::list<std::string> headers;
+            std::ostringstream os;
+            curlpp::options::WriteStream ws(&os);
+            std::string headerData = "Authorization: Bearer ";
+            std::string requestUrl = baseTwitterUrl + worldDict[i].getCountryID();
+            fileNameStream << "Twitter Trending(" << worldDict[i].getCountry() << ").txt";
+            fileName = fileNameStream.str();
+            fileNameStream.clear();
+            fileNameStream.str("");
+            headerData.append(std::getenv("BEARER_TOKEN"));
+            curlpp::Cleanup cleaner;
+            curlpp::Easy request;
+            headers.push_back(headerData);
+            request.setOpt(new HttpHeader(headers));
+            request.setOpt(new Url(requestUrl));
+            request.setOpt(ws);
+            // request.setOpt(new Verbose(true));
+            request.perform();
+
+            string responseText = os.str();
+            // cout << i << endl;
+            // cout << responseText << endl;
+            json j = json::parse(responseText);
+            string twitterTrend;
+            for (int i = 0; i < j[0]["trends"].size(); i++)
+            {
+                twitterTrend += to_string(i + 1) + ' ';
+                twitterTrend += j[0]["trends"][i]["name"];
+                twitterTrend += "    ";
+                twitterTrend += to_string(j[0]["trends"][i]["tweet_volume"]);
+                twitterTrend += "\n";
+            }
+            twitterTrend.erase(twitterTrend.size() - 1, 1);
+            // cout << twitterTrend << endl;
+            updateGist(fileName, twitterTrend);
+            // std::this_thread::sleep_for(std::chrono::seconds(3));
         }
-        twitterTrend.erase(twitterTrend.size() - 1, 1);
-        // cout << twitterTrend << endl;
-        updateGist("Twitter Trending(USA).txt", twitterTrend);
     }
     catch (curlpp::LogicError &e)
     {
